3d54a42cab69de60aa20ca642844aae13e59d315
小野晃 <onoakira@onoakiranoMacBook-Pro.local>
2019-07-16 06:55:47 +0900

    Add “user-control”,”edit-group”

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = locale
+  end
+
+  def locale
+    @locale ||= params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    options.merge(locale: locale)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
