450e4234c45e4ce21c622b6d89c2a29033d2d97f
小野晃 <onoakira@onoakiranoMacBook-Pro.local>
2019-09-21 18:06:41 +0900

    AWS
    Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.1"
+lock "3.11.1"
 
 set :application, "chat-space"
 set :repo_url, "git@example.com:kirakira33/chat-space.git"
